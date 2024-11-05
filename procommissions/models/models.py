aae73ffbf9b7c043e48e3c1b7fa51fbf2fdb4114
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-11-05 16:51:40 -0500

    Remove invoice_date

--- procommissions/models/models.py
+++ procommissions/models/models.py
@@ -9,4 +9,4 @@ class Commissions(models.Model):
     
     invoice_id = fields.Many2one('account.move', string="Invoice", domain=[('move_type', '=', 'out_invoice')], required=True)
     # partner_id = fields.Many2one(related='invoice_id.partner_id', string="Customer", store=True, readonly=True)
-    invoice_date = fields.Date(related='invoice_id.invoice_date', string="Invoice Date", store=True)
+    # invoice_date = fields.Date(related='invoice_id.invoice_date', string="Invoice Date", store=True)
