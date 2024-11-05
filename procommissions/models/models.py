ec87bc8f278d3e88e4484f410ada3479f3ee80ab
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-11-05 12:56:12 -0500

    Changes to commissions module

--- procommissions/models/models.py
+++ procommissions/models/models.py
@@ -6,3 +6,7 @@ class Commissions(models.Model):
 
     name = fields.Char(string="Commission Name", required=True)
     amount = fields.Float(string="Amount")
+    
+    invoice_id = fields.Many2one('account.move', string="Invoice", domain=[('move_type', '=', 'out_invoice')], required=True)
+    partner_id = fields.Many2one(related='invoice_id.partner_id', string="Customer", store=True)
+    invoice_date = fields.Date(related='invoice_id.invoice_date', string="Invoice Date", store=True)
