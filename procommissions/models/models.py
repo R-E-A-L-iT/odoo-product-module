8ba196718fbc7c94dd455ba4379a5cc66f65ff13
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-11-05 17:17:27 -0500

    Changed fields

--- procommissions/models/models.py
+++ procommissions/models/models.py
@@ -3,29 +3,34 @@ from odoo import models, fields, api
 class Commissions(models.Model):
     _name = 'procom.commission'
     _description = 'Commissions Records'
-
-    name = fields.Char(string="Commission Name", required=True)
-    amount = fields.Float(string="Amount")
     
-    invoice_id = fields.Many2one('account.move', string="Invoice", domain=[('move_type', '=', 'out_invoice')], required=True)
+    name = fields.Char(string="Commission Name", required=True)
+    related_lead = fields.Many2one('crm.lead', string="Related Lead")
+    related_order = fields.Many2one('sale.order', string="Related Order")
+    related_invoice = fields.Many2one('account.move', string="Related Invoice", domain=[('move_type', '=', 'out_invoice')])
+
+    # name = fields.Char(string="Commission Name", required=True)
+    # amount = fields.Float(string="Amount")
+    
+    # invoice_id = fields.Many2one('account.move', string="Invoice", domain=[('move_type', '=', 'out_invoice')], required=True)
     # partner_id = fields.Many2one(related='invoice_id.partner_id', string="Customer", store=True, readonly=True)
     # invoice_date = fields.Date(related='invoice_id.invoice_date', string="Invoice Date", store=True)
     
-    @api.model
-    def create_commission_for_all_invoices(self):
-        # Fetch all customer invoices
-        invoices = self.env['account.move'].search([('move_type', '=', 'out_invoice')])
+    # @api.model
+    # def create_commission_for_all_invoices(self):
+    #     # Fetch all customer invoices
+    #     invoices = self.env['account.move'].search([('move_type', '=', 'out_invoice')])
         
-        # Loop through the invoices and create commission records
-        for invoice in invoices:
-            # Check if a commission record already exists for the invoice
-            if not self.search([('invoice_id', '=', invoice.id)]):
-                self.create({
-                    'invoice_id': invoice.id,
-                })
+    #     # Loop through the invoices and create commission records
+    #     for invoice in invoices:
+    #         # Check if a commission record already exists for the invoice
+    #         if not self.search([('invoice_id', '=', invoice.id)]):
+    #             self.create({
+    #                 'invoice_id': invoice.id,
+    #             })
 
-    # Action to clear all commission records
-    @api.model
-    def clear_commission_records(self):
-        # Remove all records from the commission model
-        self.search([]).unlink()
+    # # Action to clear all commission records
+    # @api.model
+    # def clear_commission_records(self):
+    #     # Remove all records from the commission model
+    #     self.search([]).unlink()
