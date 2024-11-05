1a1fa77392eeb772fa8b9c9fc81400a49ee1cfbe
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-11-05 17:06:52 -0500

    Add buttons

--- procommissions/models/models.py
+++ procommissions/models/models.py
@@ -10,3 +10,22 @@ class Commissions(models.Model):
     invoice_id = fields.Many2one('account.move', string="Invoice", domain=[('move_type', '=', 'out_invoice')], required=True)
     # partner_id = fields.Many2one(related='invoice_id.partner_id', string="Customer", store=True, readonly=True)
     # invoice_date = fields.Date(related='invoice_id.invoice_date', string="Invoice Date", store=True)
+    
+    @api.model
+    def create_commission_for_all_invoices(self):
+        # Fetch all customer invoices
+        invoices = self.env['account.move'].search([('move_type', '=', 'out_invoice')])
+        
+        # Loop through the invoices and create commission records
+        for invoice in invoices:
+            # Check if a commission record already exists for the invoice
+            if not self.search([('invoice_id', '=', invoice.id)]):
+                self.create({
+                    'invoice_id': invoice.id,
+                })
+
+    # Action to clear all commission records
+    @api.model
+    def clear_commission_records(self):
+        # Remove all records from the commission model
+        self.search([]).unlink()
