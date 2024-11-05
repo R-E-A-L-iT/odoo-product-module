6d6d2e3f481fd07776719212e55bc8a0c2a8b46f
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-11-05 17:25:23 -0500

    Update models.py

--- procommissions/models/models.py
+++ procommissions/models/models.py
@@ -6,7 +6,7 @@ class Commissions(models.Model):
     
     name = fields.Char(string="Commission Name", required=True)
     related_lead = fields.Many2one('crm.lead', string="Related Lead")
-    related_order = fields.Many2one('sale.order', string="Related Order" required="True")
+    related_order = fields.Many2one('sale.order', string="Related Order", required="True")
     # domain=[('move_type', '=', 'out_invoice')]
     related_invoice = fields.Many2one('account.move', string="Related Invoice")
     
