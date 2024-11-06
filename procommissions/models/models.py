5d0886a3505eb32d05e53f538440e23d4d0ce3a8
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-11-06 15:25:35 -0500

    Restored fields

--- procommissions/models/models.py
+++ procommissions/models/models.py
@@ -10,12 +10,12 @@ class Commissions(models.Model):
     # domain=[('move_type', '=', 'out_invoice')]
     related_invoice = fields.Many2one('account.move', string="Related Invoice")
     
-    test_user = fields.Many2one('res.users', string="Assigned User")
+    # test_user = fields.Many2one('res.users', string="Assigned User")
     
     # sales roles
-    # new_customer = fields.Many2one('res.user', string="Contact New Customer")
-    # logged_lead = fields.Many2one('res.user', string="Logged Lead")
-    # developed_lead = fields.Many2one('res.user', string="Developed Opportunity")
-    # performed_demo = fields.Many2one('res.user', string="Performed Demo")
-    # quote_to_order = fields.Many2one('res.user', string="Quote to Order")
+    new_customer = fields.Many2one('res.users', string="Contact New Customer")
+    logged_lead = fields.Many2one('res.users', string="Logged Lead")
+    developed_lead = fields.Many2one('res.users', string="Developed Opportunity")
+    performed_demo = fields.Many2one('res.users', string="Performed Demo")
+    quote_to_order = fields.Many2one('res.users', string="Quote to Order")
     
