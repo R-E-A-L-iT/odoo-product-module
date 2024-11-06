16515493e5da08f1ca2eaf54da2ca7203c110052
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-11-06 14:58:20 -0500

    Added salesperson fields

--- procommissions/models/models.py
+++ procommissions/models/models.py
@@ -9,3 +9,11 @@ class Commissions(models.Model):
     related_order = fields.Many2one('sale.order', string="Related Order", required="True")
     # domain=[('move_type', '=', 'out_invoice')]
     related_invoice = fields.Many2one('account.move', string="Related Invoice")
+    
+    # sales roles
+    new_customer = fields.Many2one('res.user', string="Contact New Customer")
+    logged_lead = fields.Many2one('res.user', string="Logged Lead")
+    developed_lead = fields.Many2one('res.user', string="Developed Opportunity")
+    preformed_demo = fields.Many2one('res.user', string="Performed Demo")
+    quote_to_order = fields.Many2one('res.user', string="Quote to Order")
+    
