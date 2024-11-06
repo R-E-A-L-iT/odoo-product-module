61a22ecf6094586f983bba7b640301c3039f00f8
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-11-06 15:45:25 -0500

    Add computation fields

--- procommissions/models/models.py
+++ procommissions/models/models.py
@@ -10,8 +10,6 @@ class Commissions(models.Model):
     # domain=[('move_type', '=', 'out_invoice')]
     related_invoice = fields.Many2one('account.move', string="Related Invoice")
     
-    # test_user = fields.Many2one('res.users', string="Assigned User")
-    
     # sales roles
     new_customer = fields.Many2one('res.users', string="Contact New Customer")
     logged_lead = fields.Many2one('res.users', string="Logged Lead")
@@ -26,15 +24,32 @@ class Commissions(models.Model):
         store=True
     )
 
+    # commission fields
     new_customer_commission = fields.Monetary(string="New Customer Commission", currency_field="currency_id")
     logged_lead_commission = fields.Monetary(string="Logged Lead Commission", currency_field="currency_id")
     developed_lead_commission = fields.Monetary(string="Developed Opportunity Commission", currency_field="currency_id")
     performed_demo_commission = fields.Monetary(string="Performed Demo Commission", currency_field="currency_id")
     quote_to_order_commission = fields.Monetary(string="Quote to Order Commission", currency_field="currency_id")
+    
+    # computation fields
+    sales_price = fields.Monetary(string="Sales Price (before tax)", currency_field="currency_id", compute="_compute_sales_price", store=True)
+    reality_cost = fields.Monetary(string="Reality Cost", currency_field="currency_id")
+    shipping_cost = fields.Monetary(string="Shipping Cost", currency_field="currency_id")
+    reality_margin = fields.Monetary(string="Reality Margin", currency_field="currency_id", compute="_compute_reality_margin", store=True)
 
     @api.depends('related_order')
     def _compute_currency_id(self):
         default_currency = self.env.ref('base.CAD')
         for record in self:
             record.currency_id = record.related_order.currency_id or default_currency
+            
+    @api.depends('related_order')
+    def _compute_sales_price(self):
+        for record in self:
+            record.sales_price = record.related_order.amount_untaxed if record.related_order else 0.0
+
+    @api.depends('sales_price', 'reality_cost', 'shipping_cost')
+    def _compute_reality_margin(self):
+        for record in self:
+            record.reality_margin = record.sales_price - record.reality_cost - record.shipping_cost
     
