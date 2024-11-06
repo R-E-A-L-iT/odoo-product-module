8f62384f510748d371311a6f92703f5f68b4ef97
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-11-06 15:20:58 -0500

    removed fields

--- procommissions/models/models.py
+++ procommissions/models/models.py
@@ -11,9 +11,9 @@ class Commissions(models.Model):
     related_invoice = fields.Many2one('account.move', string="Related Invoice")
     
     # sales roles
-    new_customer = fields.Many2one('res.user', string="Contact New Customer")
-    logged_lead = fields.Many2one('res.user', string="Logged Lead")
-    developed_lead = fields.Many2one('res.user', string="Developed Opportunity")
-    performed_demo = fields.Many2one('res.user', string="Performed Demo")
-    quote_to_order = fields.Many2one('res.user', string="Quote to Order")
+    # new_customer = fields.Many2one('res.user', string="Contact New Customer")
+    # logged_lead = fields.Many2one('res.user', string="Logged Lead")
+    # developed_lead = fields.Many2one('res.user', string="Developed Opportunity")
+    # performed_demo = fields.Many2one('res.user', string="Performed Demo")
+    # quote_to_order = fields.Many2one('res.user', string="Quote to Order")
     
