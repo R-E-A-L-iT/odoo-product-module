5d674429e103c49e7f43c90c92928a053c8344e8
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-11-06 13:57:41 -0500

    Update models.py

--- procommissions/models/models.py
+++ procommissions/models/models.py
@@ -10,6 +10,20 @@ class Commissions(models.Model):
     # domain=[('move_type', '=', 'out_invoice')]
     related_invoice = fields.Many2one('account.move', string="Related Invoice")
     
+    @api.onchange('related_order')
+    def _onchange_related_order(self):
+        """Auto-fill or clear dependent fields based on related_order selection."""
+        if self.related_order:
+            _logger.info(f"Setting related fields based on related_order: {self.related_order.id}")
+            # If related_order is selected, populate other fields as needed
+            # For example, partner_id if it were still included:
+            # self.partner_id = self.related_order.partner_id
+        else:
+            _logger.info("Clearing dependent fields since related_order is empty.")
+            # Clear any fields that depend on related_order if it's empty
+            self.related_invoice = False
+            self.related_lead = False
+    
     # related_partner = fields.Many2one('res.partner', string="Customer", readonly=True)
     
     # @api.onchange('related_order')
