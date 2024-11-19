421983b4f4476b7d315232085f614529bc3a7a41
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-11-19 15:19:05 -0500

    Update models.py

--- proexpenses/models/models.py
+++ proexpenses/models/models.py
@@ -5,10 +5,8 @@ class AccountBankStatementLine(models.Model):
     _inherit = 'account.bank.statement.line'
 
     def action_transfer_expense(self):
-        # Placeholder function for the button action
-        self.ensure_one()  # Ensure the method is called on a single record
+        self.ensure_one()
 
-        # Get or create the company partner
         partner = self.env['res.partner'].search([
             ('name', '=', 'R-E-A-L.iT U.S. Inc'),
             ('company_id', '=', self.company_id.id)
@@ -20,18 +18,16 @@ class AccountBankStatementLine(models.Model):
                 'is_company': True,
             })
 
-        # Create the invoice
         invoice = self.env['account.move'].create({
             'partner_id': partner.id,
-            'move_type': 'in_invoice',  # Vendor Bill
+            'move_type': 'out_invoice',
             'company_id': self.company_id.id,
             'invoice_date': fields.Date.context_today(self),
         })
 
-        # Return an action to open the invoice
         return {
             'type': 'ir.actions.act_window',
-            'name': _('Vendor Bill'),
+            'name': _('Customer Invoice'),
             'res_model': 'account.move',
             'view_mode': 'form',
             'res_id': invoice.id,
