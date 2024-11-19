2b587f91cd464579a4ac1581aa59feade1e48b8c
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-11-19 13:11:28 -0500

    Add ProExpenses module

---
+++ proexpenses/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'ProExpenses',
+    'version': '1.0',
+    'summary': 'Module to make the transfer and management of inter-company expenses more efficient',
+    'category': 'Hidden',
+    'author': 'Ezekiel J. deBlois',
+    'depends': ['base'],
+    'data': [
+        'views/bank_statement_views.xml',
+    ],
+    'application': False,
+    'installable': True,
+}
