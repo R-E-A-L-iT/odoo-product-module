db864298bab5821038658df35337d2d5ddc9da89
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-11-19 16:01:28 -0500

    Update models.py

--- proexpenses/models/models.py
+++ proexpenses/models/models.py
@@ -17,13 +17,38 @@ class AccountBankStatementLine(models.Model):
                 'company_id': self.company_id.id,
                 'is_company': True,
             })
+            
+        inter_company_account = self.env['account.account'].search([
+            ('name', '=', 'Inter-Company Expenses'),
+            ('company_id', '=', self.company_id.id)
+        ], limit=1)
+        if not inter_company_account:
+            inter_company_account = self.env['account.account'].create({
+                'name': 'Inter-Company Expenses',
+                'code': '99999',  # Example account code; adjust as needed
+                'user_type_id': self.env.ref('account.data_account_type_expenses').id,
+                'company_id': self.company_id.id,
+            })
 
         invoice = self.env['account.move'].create({
             'partner_id': partner.id,
-            'move_type': 'out_invoice',
+            'move_type': 'out_invoice',  # Customer Invoice
             'company_id': self.company_id.id,
             'invoice_date': fields.Date.context_today(self),
         })
+        
+        self.env['account.move.line'].create({
+            'move_id': invoice.id,
+            'account_id': inter_company_account.id,
+            'name': 'Inter-Company Expense',  # Description for the line
+            'quantity': 1.0,
+            'price_unit': 0.0,  # No cost by default
+        })
+        
+        note_message = _(
+            "Invoice automatically generated as a transferred expense from %s"
+        ) % (self.display_name or _("Unknown Document"))
+        invoice.message_post(body=note_message)
 
         return {
             'type': 'ir.actions.act_window',
