6f8160e3d38cf293bdcd48b5074d763c7c668848
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-11-25 11:30:49 -0500

    Revert "Add ProExpenses module"
    This reverts commit 37183ea97726fb2dc9a92e89f52d76b5dbf60876.

--- proexpenses/models/models.py
+++
@@ -1,9 +0,0 @@
-# pro_expenses/models/bank_statement_line.py
-from odoo import models, fields
-
-class AccountBankStatementLine(models.Model):
-    _inherit = 'account.bank.statement.line'
-
-    def action_transfer_expense(self):
-        # Placeholder function for the button action
-        return True
