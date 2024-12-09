c517df4a2e6bf1bab8da861999b20ba7f37dd464
Ezekiel J. deBlois <ezekiel@deblois.me>
2024-12-09 11:51:48 -0700

    Add more logs

--- prosite/static/src/js/header.js
+++ prosite/static/src/js/header.js
@@ -1,21 +1,27 @@
 console.log("header.js file loaded");
 
 document.addEventListener("DOMContentLoaded", function () {
-  const menuItems = document.querySelectorAll(".menu-item");
-  const submenus = document.querySelectorAll(".submenu");
+    const menuItems = document.querySelectorAll(".menu-item");
+    const submenus = document.querySelectorAll(".submenu");
 
-  menuItems.forEach((item) => {
-    item.addEventListener("mouseenter", function () {
-      const submenuId = item.getAttribute("data-submenu");
-      submenus.forEach((submenu) => {
-        submenu.style.display = submenu.id === submenuId ? "flex" : "none";
-      });
+    menuItems.forEach((item) => {
+        item.addEventListener("mouseenter", function () {
+            console.log("mouseenter event listener added");
+            const submenuId = item.getAttribute("data-submenu");
+            submenus.forEach((submenu) => {
+                submenu.style.display = submenu.id === submenuId ? "flex" : "none";
+                console.log("mouseenter triggered");
+            });
+        });
     });
-  });
 
-  document
-    .querySelector("#custom-navbar")
-    .addEventListener("mouseleave", () => {
-      submenus.forEach((submenu) => (submenu.style.display = "none"));
+    document
+        .querySelector("#custom-navbar")
+        .addEventListener("mouseleave", () => {
+            console.log("mouseleave event listener added");
+            submenus.forEach((submenu) => {
+                submenu.style.display = "none";
+                console.log("mouseenter triggered");
+            });
+        });
     });
-});
